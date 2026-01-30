47e78d28819b70b6049f950967560f650a402e3b
HipNg08 <hiep09012008@gmail.com>
2026-01-30 14:36:54 +0700

    menu
    tạo menu asm 1+2

---
+++ ASM.py
@@ -0,0 +1,17 @@
+task = []
+while True:
+      
+        print("\n" + "*"*30)
+        print("  HỆ THỐNG QUẢN LÝ LAPTOP POLY-LAP")
+        print("*"*30)
+        print("1. Hiển thị danh sách sản phẩm")
+        print("2. Thêm sản phẩm mới")
+        print("3. Tìm kiếm sản phẩm theo tên")
+        print("4. Cập nhật thông tin sản phẩm")
+        print("5. Xóa sản phẩm")
+        print("6. Lưu dữ liệu & Thoát")
+        print("*"*30)
+        
+        choice = input("Mời bạn chọn chức năng (1-6): ")
+
+
